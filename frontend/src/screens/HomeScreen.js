e27c1278a743072f8311ed4f1e0bb19eb041f40b
Amine Essbaa <amineessbaa2020@gmail.com>
2023-01-24 19:08:17 +0100

    1

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -18,10 +18,22 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{loading, error,products },dispatch] = useReducer(logger,(reducer),{
+    products: [],
+    loading : true,
+    error : '',
+  
+  });
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
+      dispatch({type: 'FETCH_REQUEST'});
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({type: 'FETCH_SUCCESS',payload: result.data});
+      }catch(err) {
+        dispatch({type: 'FETCH__FAIL',payload:err.message});
+      }
+  
       setProducts(result.data);
     };
     fetchData();
@@ -30,7 +42,12 @@ function HomeScreen() {
     <div>
       <h>list product</h>
       <div className="products">
-        {data.products.map((product) => (
+        {loading ? (
+        <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+        products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={'/product/${product.skug}'}>
               <img src={product.image} alt={product.name} />
